467a97bf3c4d96d9ad56842f5aa4d69bd70b9cda
chenyongqin <chenyongqin@dataknown.cn>
2021-12-22 16:51:31 +0800

    打印报错

--- run.py
+++ run.py
@@ -142,5 +142,8 @@ def auto_token_load():
 
 # 运行
 if __name__ == "__main__":
-    ThreadPoolExecutor(2).submit(auto_token_load)
+    try:
+        ThreadPoolExecutor(2).submit(auto_token_load)
+    except Exception as e:
+        print(e)
     app.run("0.0.0.0", port=5000)
